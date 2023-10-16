3155b79bb92a71a883f0ab4826f2c7a5633a632c
Keegan White <keeganthomaswhite@gmail.com>
2023-10-16 15:38:44 +0200

    Init project code

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
-# esp32-s3-webserver-led
-A basic web server hosted on an ESP32-S3 board that controls an LED.
+# ESP32 S3 Webserver LED
+This repo contains the code to build and run an ESP32 S3 board as a webserver and control an LED.
+
+## Part List
+- [ESP32 S3 Micro Controller](https://www.pishop.co.za/store/esp32-s3-microcontroller-24-ghz-wi-fi-development-board-dual-core-processor-with-frequency-up-to-240-mhz?keyword=esp32&category_id=0)
+- [Breadboard](https://www.pishop.co.za/store/830-point-solderless-breadboard?keyword=830%20Point%20Solderless%20Breadboard&category_id=0)
+- [Jumper Cables](https://www.pishop.co.za/store/120pcs-20cm-color-breadboard-jumper-cable-wire-kit?keyword=jumper%20cable&category_id=0)
+- [LED](https://www.pishop.co.za/store/leds/10mm-rgb-led-module)
+
+## Demo
+1. Connect the 3.3V out pin to your LED on the pin labeled 'V'.
+2. Connect the group pin of the ESP32 board to the negative rail on your breadboard.
+3. Connect any GPIO pin to the pin labeled by an 'R' on the LED. _I am using pin 1 in mode code._
+4. Power the board
+5. Flash the board, full instructions [here](#running-code).
+6. Connect to the WiFi network named `esp32_wifi` the board will provide with password: `password`.
+
+### Full Set Up
+Full set up described above:
+
+<img alt="full-setup" height="400" src="images/full-setup.jpg" width="200"/>
+
+### Pins
+Pin layout described above:
+
+<img alt="pins" height="400" src="images/pins.jpg" width="200"/>
+
+### WiFi Network 
+The WiFi network will show up like this:
+
+![wifi-network](images/wifi-network.png)
+### Web Page
+#### LED ON STATE
+The Webpage will look like this once you are connected and navigate to [http://192.168.4.1/ledon](http://192.168.4.1/ledon):
+![led-on](images/led-on.png)
+#### LED OFF STATE
+The Webpage will look like this once you are connected and navigate to [http://192.168.4.1/ledoff](http://192.168.4.1/ledoff):
+![led-off](images/led-off.png)
+
+## Running Code
+You can use the ESP IDF plugin for VS Code to easily build your code, and flash and monitor your device. However, here 
+are the manual lines of code that you can run. Ensure you have your environment variables set for this to work or 
+use absolute paths.
+1. Clean the project: `idf.py fullclean`
+2. Build the project `idf.py build`
+3. Set target chip: `idf.py set-target esp32s3`
+4. Flask the binary to the board: `idf.py -p <PORT> flash` where you can check the serial ports on Ubuntu: 
+`sudo dmesg | grep tty`. For example: `idf.py -p /dev/ttyACM1 flash`
+5. Monitor device output: `idf.py -p <PORT> monitor`
+
+## Customisation
+- In the [sdkconfig](sdkconfig) file you can change your SSID and Password in the section:
+```
+# Example Configuration
+#
+CONFIG_ESP_WIFI_SSID="esp32_wifi"
+CONFIG_ESP_WIFI_PASSWORD="password"
+CONFIG_ESP_WIFI_CHANNEL=1
+CONFIG_ESP_MAX_STA_CONN=4
+# end of Example Configuration
+```
---
+++ main/main.c
@@ -0,0 +1,303 @@
+/*  WiFi softAP Example
+
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_mac.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+#include "driver/gpio.h"
+#include "lwip/err.h"
+#include "lwip/sys.h"
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/param.h>
+#include "esp_netif.h"
+#include <esp_http_server.h>
+#include "esp_tls.h"
+#if !CONFIG_IDF_TARGET_LINUX
+#include <esp_system.h>
+
+#endif  // !CONFIG_IDF_TARGET_LINUX
+
+static const char *TAG = "webserver";
+
+/* The examples use WiFi configuration that you can set via project configuration menu.
+
+   If you'd rather not, just change the below entries to strings with
+   the config you want - ie #define EXAMPLE_WIFI_SSID "mywifissid"
+*/
+#define EXAMPLE_ESP_WIFI_SSID      CONFIG_ESP_WIFI_SSID
+#define EXAMPLE_ESP_WIFI_PASS      CONFIG_ESP_WIFI_PASSWORD
+#define EXAMPLE_ESP_WIFI_CHANNEL   CONFIG_ESP_WIFI_CHANNEL
+#define EXAMPLE_MAX_STA_CONN       CONFIG_ESP_MAX_STA_CONN
+#define GPIO_RED    GPIO_NUM_1 
+
+static esp_err_t ledon_handler(httpd_req_t *req)
+{
+    esp_err_t error;
+    gpio_set_level(GPIO_RED, 0);
+    ESP_LOGI(TAG, "LED Turned ON");
+    const char *response = (const char *) req->user_ctx;
+    error= httpd_resp_send(req, response, strlen(response));
+    if(error != ESP_OK)
+    {
+        ESP_LOGI(TAG, "ERROR %d while sending Response", error);
+    }
+    else ESP_LOGI(TAG, "Response sent successfully");
+    return error;
+}
+
+static const httpd_uri_t ledon = {
+    .uri       = "/ledon",
+    .method    = HTTP_GET,
+    .handler   = ledon_handler,
+    .user_ctx  = "<!DOCTYPE html>"\
+                 "<html>"\
+                 "<head>"\
+                 "<style>"\
+                 ".button {"\
+                 "  border: none;"\
+                 "  color: white;"\
+                 "  padding: 15px 32px;"\
+                 "  text-align: center;"\
+                 "  text-decoration: none;"\
+                 "  display: inline-block;"\
+                 "  font-size: 16px;"\
+                 "  margin: 4px 2px;"\
+                 "  cursor: pointer;"\
+                 "}"\
+                 ".button1 {background-color: #000000;}"\
+                 "</style>"\
+                 "</head>"\
+                 "<body>"\
+                 "<h1>ESP32-S3 Webser</h1>"\
+                 "<p>Change LED State</p>"\
+                 "<h3>LED STATE: ON<h3>"\
+                 "<button class=\"button button1\" onclick = \"window.location.href='/ledoff'\">TURN LED OFF</button>"\
+                 "</body>"\
+                 "</html>"
+};
+
+
+/* An HTTP GET handler */
+static esp_err_t ledoff_handler(httpd_req_t *req)
+{
+    esp_err_t error;
+    gpio_set_level(GPIO_RED, 1);
+    ESP_LOGI(TAG, "LED Turned Off");
+    const char *response = (const char *) req->user_ctx;
+    error= httpd_resp_send(req, response, strlen(response));
+    if(error != ESP_OK)
+    {
+        ESP_LOGI(TAG, "ERROR %d while sending Response", error);
+    }
+    else ESP_LOGI(TAG, "Response sent successfully");
+    return error;
+}
+
+static const httpd_uri_t ledoff = {
+    .uri       = "/ledoff",
+    .method    = HTTP_GET,
+    .handler   = ledoff_handler,
+    .user_ctx  = "<!DOCTYPE html>"\
+                 "<html>"\
+                 "<head>"\
+                 "<style>"\
+                 ".button {"\
+                 "  border: none;"\
+                 "  color: white;"\
+                 "  padding: 15px 32px;"\
+                 "  text-align: center;"\
+                 "  text-decoration: none;"\
+                 "  display: inline-block;"\
+                 "  font-size: 16px;"\
+                 "  margin: 4px 2px;"\
+                 "  cursor: pointer;"\
+                 "}"\
+                 ".button1 {background-color: #4CAF50;}"\
+                 "</style>"\
+                 "</head>"\
+                 "<body>"\
+                 "<h1>ESP32-S3 Webser</h1>"\
+                 "<p>Change LED State</p>"\
+                 "<h3>LED STATE: OFF<h3>"\
+                 "<button class=\"button button1\" onclick = \"window.location.href='/ledon'\">TURN LED ON</button>"\
+                 "</body>"\
+                 "</html>"
+};
+
+
+/* This handler allows the custom error handling functionality to be
+ * tested from client side. For that, when a PUT request 0 is sent to
+ * URI /ctrl, the /hello and /echo URIs are unregistered and following
+ * custom error handler http_404_error_handler() is registered.
+ * Afterwards, when /hello or /echo is requested, this custom error
+ * handler is invoked which, after sending an error message to client,
+ * either closes the underlying socket (when requested URI is /echo)
+ * or keeps it open (when requested URI is /hello). This allows the
+ * client to infer if the custom error handler is functioning as expected
+ * by observing the socket state.
+ */
+esp_err_t http_404_error_handler(httpd_req_t *req, httpd_err_code_t err)
+{
+
+    /* For any other URI send 404 and close socket */
+    httpd_resp_send_err(req, HTTPD_404_NOT_FOUND, "Some 404 error message");
+    return ESP_FAIL;
+}
+
+
+static httpd_handle_t start_webserver(void)
+{
+    httpd_handle_t server = NULL;
+    httpd_config_t config = HTTPD_DEFAULT_CONFIG();
+#if CONFIG_IDF_TARGET_LINUX
+    // Setting port as 8001 when building for Linux. Port 80 can be used only by a priviliged user in linux.
+    // So when a unpriviliged user tries to run the application, it throws bind error and the server is not started.
+    // Port 8001 can be used by an unpriviliged user as well. So the application will not throw bind error and the
+    // server will be started.
+    config.server_port = 8001;
+#endif // !CONFIG_IDF_TARGET_LINUX
+    config.lru_purge_enable = true;
+
+    // Start the httpd server
+    ESP_LOGI(TAG, "Starting server on port: '%d'", config.server_port);
+    
+    if (httpd_start(&server, &config) == ESP_OK) {
+        // Set URI handlers
+        ESP_LOGI(TAG, "Registering URI handlers");
+        httpd_register_uri_handler(server, &ledoff);
+        httpd_register_uri_handler(server, &ledon);
+        return server;
+    }
+
+    ESP_LOGI(TAG, "Error starting server!");
+    return NULL;
+}
+
+#if !CONFIG_IDF_TARGET_LINUX
+static esp_err_t stop_webserver(httpd_handle_t server)
+{
+    // Stop the httpd server
+    return httpd_stop(server);
+}
+
+static void disconnect_handler(void* arg, esp_event_base_t event_base,
+                               int32_t event_id, void* event_data)
+{
+    httpd_handle_t* server = (httpd_handle_t*) arg;
+    if (*server) {
+        ESP_LOGI(TAG, "Stopping webserver");
+        if (stop_webserver(*server) == ESP_OK) {
+            *server = NULL;
+        } else {
+            ESP_LOGE(TAG, "Failed to stop http server");
+        }
+    }
+}
+
+static void connect_handler(void* arg, esp_event_base_t event_base,
+                            int32_t event_id, void* event_data)
+{
+    httpd_handle_t* server = (httpd_handle_t*) arg;
+    if (*server == NULL) {
+        ESP_LOGI(TAG, "Starting webserver");
+        *server = start_webserver();
+    }
+}
+#endif // !CONFIG_IDF_TARGET_LINUX
+
+static void wifi_event_handler(void* arg, esp_event_base_t event_base,
+                                    int32_t event_id, void* event_data)
+{
+    if (event_id == WIFI_EVENT_AP_STACONNECTED) {
+        wifi_event_ap_staconnected_t* event = (wifi_event_ap_staconnected_t*) event_data;
+        ESP_LOGI(TAG, "station "MACSTR" join, AID=%d",
+                 MAC2STR(event->mac), event->aid);
+    } else if (event_id == WIFI_EVENT_AP_STADISCONNECTED) {
+        wifi_event_ap_stadisconnected_t* event = (wifi_event_ap_stadisconnected_t*) event_data;
+        ESP_LOGI(TAG, "station "MACSTR" leave, AID=%d",
+                 MAC2STR(event->mac), event->aid);
+    }
+}
+
+void wifi_init_softap(void)
+{
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    esp_netif_create_default_wifi_ap();
+
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
+                                                        ESP_EVENT_ANY_ID,
+                                                        &wifi_event_handler,
+                                                        NULL,
+                                                        NULL));
+
+    wifi_config_t wifi_config = {
+        .ap = {
+            .ssid = EXAMPLE_ESP_WIFI_SSID,
+            .ssid_len = strlen(EXAMPLE_ESP_WIFI_SSID),
+            .channel = EXAMPLE_ESP_WIFI_CHANNEL,
+            .password = EXAMPLE_ESP_WIFI_PASS,
+            .max_connection = EXAMPLE_MAX_STA_CONN,
+#ifdef CONFIG_ESP_WIFI_SOFTAP_SAE_SUPPORT
+            .authmode = WIFI_AUTH_WPA3_PSK,
+            .sae_pwe_h2e = WPA3_SAE_PWE_BOTH,
+#else /* CONFIG_ESP_WIFI_SOFTAP_SAE_SUPPORT */
+            .authmode = WIFI_AUTH_WPA2_PSK,
+#endif
+            .pmf_cfg = {
+                    .required = true,
+            },
+        },
+    };
+    if (strlen(EXAMPLE_ESP_WIFI_PASS) == 0) {
+        wifi_config.ap.authmode = WIFI_AUTH_OPEN;
+    }
+
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_AP));
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_AP, &wifi_config));
+    ESP_ERROR_CHECK(esp_wifi_start());
+
+    ESP_LOGI(TAG, "wifi_init_softap finished. SSID:%s password:%s channel:%d",
+             EXAMPLE_ESP_WIFI_SSID, EXAMPLE_ESP_WIFI_PASS, EXAMPLE_ESP_WIFI_CHANNEL);
+}
+
+static void configure_led(void)
+{
+    gpio_reset_pin(GPIO_RED);
+    gpio_set_direction(GPIO_RED, GPIO_MODE_OUTPUT);
+    gpio_set_level(GPIO_RED, 1);
+}
+
+void app_main(void)
+{
+    configure_led();
+    static httpd_handle_t server = NULL;
+    //Initialize NVS
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+      ESP_ERROR_CHECK(nvs_flash_erase());
+      ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+
+    ESP_LOGI(TAG, "ESP_WIFI_MODE_AP");
+    wifi_init_softap();
+
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_AP_STAIPASSIGNED, &connect_handler, &server));
+    // ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, WIFI_EVENT_STA_DISCONNECTED, &disconnect_handler, &server));
+}
